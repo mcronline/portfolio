2628804e9c492732910af0312422d8968714d212
Michel Rozatti <contact@michelcurti.dev>
2019-07-15 12:05:49 -0300

    changed data from json to js to import images

---
+++ src/data.js
@@ -0,0 +1,50 @@
+export default {
+    "resume" : [
+        "> Computer Engineering and Information Security degree",
+        "> More than 25 years of programming experience",
+        "> Full stack web developer / Internet Entrepreneur",
+        "Backend : Node.js; next(), Python; next(), PHP",
+        "Frontend => Javascript, React, Jquery",
+        "UI : { HTML ; SCSS }",
+        "Wordpress Developer Expert"        
+    ],
+    "portfolio" : [
+        {
+            "key":1,
+            "title" : "Imóveis Expert",
+            "skills" : "Javascript, Bootstrap, Jquery, PHP, MySQL, Wordpress",
+            "description" : "Personal project focused on real state agencies and realtors needs, delivering real state management and CRM.",
+            "img" : require("./images/imoveis_expert_portfolio.jpg")
+        },
+        {
+            "key":2,
+            "title" : "Imóveis Expert",
+            "skills" : "Javascript, Bootstrap, Jquery, PHP, MySQL, Wordpress",
+            "description" : "Personal project focused on real state agencies and realtors needs, delivering real state management and CRM.",
+            "img" : require("./images/imoveis_expert_portfolio.jpg")
+                        
+        },
+        {
+            "key":3,
+            "title" : "Imóveis Expert",
+            "skills" : "Javascript, Bootstrap, Jquery, PHP, MySQL, Wordpress",
+            "description" : "Personal project focused on real state agencies and realtors needs, delivering real state management and CRM.",
+            "img" : require("./images/imoveis_expert_portfolio.jpg")
+            
+        },
+        {
+            "key":4,
+            "title" : "Imóveis Expert",
+            "skills" : "Javascript, Bootstrap, Jquery, PHP, MySQL, Wordpress",
+            "description" : "Personal project focused on real state agencies and realtors needs, delivering real state management and CRM.",
+            "img" : require("./images/imoveis_expert_portfolio.jpg")
+        },
+        {
+            "key":5,
+            "title" : "Imóveis Expert",
+            "skills" : "Javascript, Bootstrap, Jquery, PHP, MySQL, Wordpress",
+            "description" : "Personal project focused on real state agencies and realtors needs, delivering real state management and CRM.",
+            "img" : require("./images/imoveis_expert_portfolio.jpg")
+        }
+    ]
+}
