6b099a53a6082ecf343af941ccb98287ac9e738b
Michel Rozatti <contact@michelcurti.dev>
2019-07-16 12:17:55 -0300

    Updated the portfolio area info and images

--- src/data.js
+++ src/data.js
@@ -1,8 +1,8 @@
 export default {
     "resume" : [
-        "> Computer Engineering and Information Security degree",
-        "> More than 25 years of programming experience",
-        "> Full stack web developer / Internet Entrepreneur",
+        "Computer Engineering and Information Security degree",
+        "More than 25 years of programming experience",
+        "Full stack web developer / Internet Entrepreneur",
         "Backend : Node.js; next(), Python; next(), PHP",
         "Frontend => Javascript, React, Jquery",
         "UI : { HTML ; SCSS }",
@@ -16,35 +16,35 @@ export default {
             "description" : "Personal project focused on real state agencies and realtors needs, delivering real state management and CRM.",
             "img" : require("./images/imoveis_expert_portfolio.jpg")
         },
+        
+        {
+            "key":4,
+            "title" : "Qual o CEP?",
+            "skills" : "Javascript, Bootstrap, Jquery, PHP, MySQL",
+            "description" : "Personal project focused on address search in Brazil. Payment service integration (PagSeguro)",
+            "img" : require("./images/qualocep_portfolio.jpg")
+        },
         {
             "key":2,
-            "title" : "Imóveis Expert",
-            "skills" : "Javascript, Bootstrap, Jquery, PHP, MySQL, Wordpress",
-            "description" : "Personal project focused on real state agencies and realtors needs, delivering real state management and CRM.",
-            "img" : require("./images/imoveis_expert_portfolio.jpg")
-                        
+            "title" : "Ortoriso",
+            "skills" : "Wordpress",
+            "description" : "Website for a dentistry clinic in Brazil.",
+            "img" : require("./images/ortoriso_portfolio.jpg")
         },
         {
             "key":3,
-            "title" : "Imóveis Expert",
-            "skills" : "Javascript, Bootstrap, Jquery, PHP, MySQL, Wordpress",
-            "description" : "Personal project focused on real state agencies and realtors needs, delivering real state management and CRM.",
-            "img" : require("./images/imoveis_expert_portfolio.jpg")
+            "title" : "CR Construção",
+            "skills" : "Wordpress",
+            "description" : "Website for a construction company in Brazil.",
+            "img" : require("./images/crconstrucao_portfolio.jpg")
             
         },
         {
-            "key":4,
-            "title" : "Imóveis Expert",
-            "skills" : "Javascript, Bootstrap, Jquery, PHP, MySQL, Wordpress",
-            "description" : "Personal project focused on real state agencies and realtors needs, delivering real state management and CRM.",
-            "img" : require("./images/imoveis_expert_portfolio.jpg")
-        },
-        {
             "key":5,
-            "title" : "Imóveis Expert",
-            "skills" : "Javascript, Bootstrap, Jquery, PHP, MySQL, Wordpress",
-            "description" : "Personal project focused on real state agencies and realtors needs, delivering real state management and CRM.",
-            "img" : require("./images/imoveis_expert_portfolio.jpg")
+            "title" : "Codepen Projects",
+            "skills" : "Javascript, ReactJs",
+            "description" : "Projects for programming skills development and certification",
+            "img" : require("./images/codepen_portfolio.jpg")
         }
     ]
 }
